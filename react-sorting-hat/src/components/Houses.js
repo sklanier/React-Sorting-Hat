de14cff76b01b4ad9c391e074e7be13879b753ca
Steve Lanier <stevekentlanier@gmail.com>
2019-03-21 15:53:39 -0700

    dhfjgvhbjkg2

--- react-sorting-hat/src/components/Houses.js
+++ react-sorting-hat/src/components/Houses.js
@@ -2,28 +2,24 @@ import React, { Component } from 'react';
 
   const houseData = [
     {
-      //badge: '',
       name: 'Gryffindor',
       traits: 'bravery, nerve, chivalry, courage, daring',
       foundator: 'Godric Gryffindor',
       members: 'Harry Potter, Ron Weasley, Hermione Granger, Albus Dumbledor and Sirius Black.'
     },
     {
-      //badge: '',
       name: 'Slytherin',
       traits: 'resourcelfulnnes, cunning, ambition, determination, self-preservation.',
       foundator: 'Salazar Slytherin',
       members: 'Tom Riddle, Severus Snape, Bellatrix Lestrange, Draco Malfoy and Albus Severus Potter.'
     },
     {
-      //badge: '',
       name: 'Ravenclaw',
       traits: 'intelligence, wit, wisdom, creativity, originality, individuality, acceptance.',
       foundator: 'Rowena Ravenclaw',
       members: 'Luna Lovegood, Sybil Trelawney, Gilderoy Lockhart, Filius Flitwick and Cho Chang.'
     },
     {
-      //badge: '',
       name: 'Hufflepuff',
       traits: 'dedication, hard work, fair play, patience, kindness, tolerance, unafraid of toil, loyalty.',
       foundator: 'Helga Hufflepuff',
@@ -60,7 +56,7 @@ class Houses extends Component {
 			<div>
 				<h3>Your House is..</h3>
 				<h2>{this.props.selectedHouse}</h2>
-				<button onClick={this.props.resetState} className='waves-effect waves-light btn'>
+				<button onClick={this.props.resetState}>
 					{'< Go back'}
 				</button>
 			</div>
